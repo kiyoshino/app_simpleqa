b6e1962b160f0d7c365e0d970f29eadff0e60118
Kiyoto Shinomura <k-shinomura@kddi.com>
2013-05-04 11:31:40 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Simpleqa::Application.config.secret_token = 'ff5ce24ca0f60f0ff6bc9d24fbab3a4bf80cb1f04e75edc25ea290299c2f1795df0acb2c4f5c7eeade1a67ab8e394d2b6dbf75e3cb479f3a4295a6f8072110b2'
